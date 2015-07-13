14f0ed97fd9ec5f1e029d028d872283b9ecb9b5c
Wayne Piekarski <waynepie@google.com>
2015-07-13 14:00:14 -0700

    First implementation of reading the accelerometer and a simple UI

---
+++ common/src/main/java/net/waynepiekarski/wearsensors/Logging.java
@@ -0,0 +1,21 @@
+package net.waynepiekarski.wearsensors;
+
+import android.util.Log;
+
+public class Logging {
+
+    private static final String TAG = "WearSensors";
+
+    public static void debug (String str) {
+        Log.d (TAG, str);
+    }
+
+    public static void fatal (String str) {
+        Log.e (TAG, "FATAL ERROR: " + str);
+        RuntimeException re = new RuntimeException();
+        re.printStackTrace();
+        Log.e (TAG, "Exiting with error code 1");
+        System.exit(1);
+    }
+
+}
--- mobile/src/main/java/net/waynepiekarski/wearsensors/MainActivity.java
+++ mobile/src/main/java/net/waynepiekarski/wearsensors/MainActivity.java
@@ -1,16 +1,39 @@
 package net.waynepiekarski.wearsensors;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class MainActivity extends Activity {
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private SensorEventListener listener;
+    private int sensorType;
+    private TextView viewSensorType;
+    private TextView viewSensorDetails;
+    private TextView viewSensorAccuracy;
+    private TextView viewSensorRaw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewSensorType = (TextView)findViewById(R.id.sensorType);
+        viewSensorDetails = (TextView)findViewById(R.id.sensorDetails);
+        viewSensorAccuracy = (TextView)findViewById(R.id.sensorAccuracy);
+        viewSensorRaw = (TextView)findViewById(R.id.sensorRaw);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
     }
 
     @Override
@@ -33,5 +56,36 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        sensorType = Sensor.TYPE_ACCELEROMETER;
+        sensor = sensorManager.getDefaultSensor(sensorType);
+        Logging.debug("Opened up sensor: " + sensor);
+        viewSensorType.setText(sensor.getStringType());
+        viewSensorDetails.setText(sensor.toString());
+
+        listener = new SensorEventListener() {
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent.sensor.getType() == sensorType) {
+                    Logging.debug("Accelerometer update: " + Arrays.toString(sensorEvent.values));
+                    viewSensorRaw.setText(Arrays.toString(sensorEvent.values));
+                }
+            }
+
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                Logging.debug("Accelerometer accuracy: " + accuracy);
+                viewSensorAccuracy.setText("Accuracy=" + accuracy);
+            }
+        };
+        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(listener);
     }
 }
